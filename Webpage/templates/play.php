00b42812afd636f9aba5fbbe40e1d1a9be25d657
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-12-04 15:43:35 +0800

    Update play.php - Added home button; minor tweaks.

--- Webpage/templates/play.php
+++ Webpage/templates/play.php
@@ -10,15 +10,21 @@
         <style>
             #instructionsList {
                 list-style-type: none;      /* Removes the list dot */
+                padding-left: 0px;          /* ADDED - SHAHMIZAN */
             }
             li{
                 margin-top:10px;
+            }   
+            #homeBtn{                       /* ADDED - SHAHMIZAN */
+                display: flex;
+                align-items: center;
+                justify-content: center;
             }
         </style>
     </head>
-    <h2 class="major">Control the Car!</h2>
-    <body style="padding: 15px;">
-        
+    <h2 class="major" style="padding: 15px;">Control the Car!</h2>      <!-- MODIFIED - SHAHMIZAN -->
+    <body style="padding: 15px; float:none;">                           <!-- MODIFIED - SHAHMIZAN -->
+
         <div class="container">
 
             <!-- 1st Column -->
@@ -30,28 +36,27 @@
                     <li draggable="true">Turn Left</li>
                     <li draggable="true">Loop</li>
                 </ul>
+		        <a href="/" id="homeBtn" class="button primary" draggable="false">☰ Home</a>   <!-- ADDED - SHAHMIZAN -->
             </div>
 
             <!-- 2nd Column -->
             <div name = "fullNamesContainer" style="position: relative; margin-left: 50px; float:left;">
                 <form action="/sendInstructions" method="post">
                     <form action="/action_page.php">
-                    <textarea name="message" rows="10" cols="30">
-                    </textarea>
+                    <textarea id="instructions" name="message" rows="10" cols="30"></textarea>      <!-- ADDED (id) - SHAHMIZAN -->
                     <input type="submit" class="button primary" value="Send!">
-                    <input type="button" value="Clear" onclick="window.location.reload();">
+                    <input type="button" value="Clear" onclick="eraseText();">                      <!-- MODIFIED (onclick) - SHAHMIZAN -->
                 </form>
             </div>
 
-            <!-- 3rd Column -->
-            <div style="float:left;">
+            <!-- 3rd Column -->                                                                     <!-- OMITTED - SHAHMIZAN -->
+            <!--<div style="float:left;">
                 <img src="static/mazes/440011.png" align="middle" style="margin-left:auto; margin-right: auto; margin-top: auto; margin-bottom: auto;"/>
-            </div>
-
+            </div>-->
 
             <script type="text/javascript" src="{{ url_for('static', filename='jquery.min.js') }}"></script>
             <script type="text/javascript" src="{{ url_for('static', filename='copy.js') }}"></script>
-            
+
         </div>
     </body>
 </html>
